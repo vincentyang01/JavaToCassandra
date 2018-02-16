254a5d823bb5855ff680a539a399422ad396f119
Vincent <vincentyangcsi@gmail.com>
2018-02-16 15:59:26 -0500

    Initial commit

---
+++ src/main/java/svc/cassandra/nfs_provisioner/JavaToCassandra/App.java
@@ -0,0 +1,33 @@
+package svc.cassandra.nfs_provisioner.JavaToCassandra;
+import java.util.List;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	org.apache.log4j.BasicConfigurator.configure();
+    	
+    	String serverIP = "cassandra.cassandra.svc";
+    	String keyspace = "system";
+    	
+    	Cluster cluster = Cluster.builder()
+    	  .addContactPoints()
+    	  .build();
+
+    	Session session = cluster.connect(keyspace);
+    	String cqlStatement = "SELECT * FROM local";
+    	for (Row row : session.execute(cqlStatement)) {
+    	  System.out.println(row.toString());
+    	}
+    	String cqlStatementC = "INSERT INTO exampkeyspace.users (username, password) " + 
+                "VALUES ('Serenity', 'fa3dfQefx')";
+    }
+}
